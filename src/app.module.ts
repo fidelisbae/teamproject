fb378936eac6be8cdcbf4eb7e0bdc15f473e4e80
fidelisbae <fidelisbae@gmail.com>
2022-07-06 19:22:10 +0900

    add: gcp 배포 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,31 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { CourseModule } from './course/course.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '172.21.16.7',
+      port: 3306,
+      username: 'root',
+      password: '3565',
+      database: 'dabae-database',
+      entities: [__dirname + '/apis/*/.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: './src/commons/graphql/schema.gql',
     }),
     CourseModule,
   ],
-  controllers: [],
-  providers: [],
+  providers: [AppService],
+  controllers: [AppController]
 })
 export class AppModule {}
