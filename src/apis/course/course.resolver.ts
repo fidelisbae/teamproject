e8e4c64d8230ae0a69b0691aab9e74bf6fd708c6
fidelisbae <fidelisbae@gmail.com>
2022-07-07 21:11:56 +0900

    추가: 유저테이블

--- src/apis/course/course.resolver.ts
+++
@@ -1,8 +0,0 @@
-import { Query, Resolver } from '@nestjs/graphql';
-import {} from '../user/entities/user.entity';
-import { CourseService } from './course.service';
-
-@Resolver()
-export class CourseResolver {
-  constructor(private readonly courseService: CourseService) {}
-}
