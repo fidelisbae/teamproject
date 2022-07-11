0cd65b5286390d38d629ddad72857b19cd74d42d
이샘미 <isaemmi@isaemmiui-MacBookPro.local>
2022-07-11 17:38:43 +0900

    쓸모없는 코드 삭제

--- src/apis/user/user.service.ts
+++ src/apis/user/user.service.ts
@@ -1,8 +1,4 @@
-import {
-  ConflictException,
-  Injectable,
-  UnprocessableEntityException,
-} from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
