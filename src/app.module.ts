8fbdc4bf76b6bc091b4a060d06edc79dc4cccd20
이샘미 <isaemmi@isaemmiui-MacBookPro.local>
2022-07-14 16:58:35 +0900

    proxy변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,50 +22,54 @@ import { PickModule } from './apis/pick/pick.module';
 
 @Module({
   imports: [
-    //로컬 mySQL
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'dabae-database',
-      entities: [__dirname + '/apis/**/*.entity.*'],
-      synchronize: true,
-      logging: true,
-    }),
-
-    // 배포 mySQL
+    // //로컬 mySQL
     // TypeOrmModule.forRoot({
     //   type: 'mysql',
-    //   host: '10.86.0.2',
+    //   host: '127.0.0.1',
     //   port: 3306,
     //   username: 'root',
-    //   password: '12345',
+    //   password: 'root',
     //   database: 'dabae-database',
     //   entities: [__dirname + '/apis/**/*.entity.*'],
     //   synchronize: true,
     //   logging: true,
     // }),
 
+    // 배포 mySQL
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '10.86.0.2',
+      port: 3306,
+      username: 'root',
+      password: '12345',
+      database: 'dabae-database',
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
+
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: './src/common/graphql/schema.gql',
       context: ({ req, res }) => ({ req, res }),
+      cors: {
+        origin: 'http://localhost:3000',
+        credentials: true,
+      },
     }),
-    //  로컬 Redis
-    CacheModule.register<RedisClientOptions>({
-      store: redisStore,
-      url: 'redis://localhost:6379',
-      isGlobal: true,
-    }),
-
-    // // 배포 Redis
+    // //  로컬 Redis
     // CacheModule.register<RedisClientOptions>({
     //   store: redisStore,
-    //   url: 'redis://10.86.1.3:6379',
+    //   url: 'redis://localhost:6379',
     //   isGlobal: true,
     // }),
+
+    // // 배포 Redis
+    CacheModule.register<RedisClientOptions>({
+      store: redisStore,
+      url: 'redis://10.86.1.3:6379',
+      isGlobal: true,
+    }),
 
     ReviewModule,
     UserModule,
--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(graphqlUploadExpress());
   await app.listen(3000);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
 }
 bootstrap();
