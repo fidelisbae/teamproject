5d4a414f53c48ea978737410bf39eaef9a7d5cc9
이샘미 <isaemmi@isaemmiui-MacBookPro.local>
2022-07-17 16:22:43 +0900

    payment 수정

--- src/apis/payment/payment.resolver.ts
+++ src/apis/payment/payment.resolver.ts
@@ -1,9 +1,11 @@
-import { UnauthorizedException } from '@nestjs/common';
+import { UnauthorizedException, UseGuards } from '@nestjs/common';
 import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Payment } from './entities/payment.entity';
 import { PaymentService } from './payment.service';
 import * as jwt from 'jsonwebtoken';
 import { CreatePaymentInput } from './dto/createPayment.input';
+import { GqlAuthAccessGuard } from 'src/common/auth/gql.auth.guard';
+import { CurrentUser } from 'src/common/auth/gql.user.param';
 @Resolver()
 export class PaymentResolver {
   constructor(
@@ -21,26 +23,20 @@ export class PaymentResolver {
     return await this.paymentService.findOne({ email });
   }
 
+  @UseGuards(GqlAuthAccessGuard)
   @Mutation(() => Payment)
   async createPayment(
-    @Args('createPaymentInput') createPaymentInput: CreatePaymentInput, //
-    @Context() context: any,
+    @Args('createPaymentInput') createPaymentInput: CreatePaymentInput,
+    @CurrentUser() loginUser: any,
   ) {
-    try {
-      const accessToken = context.req.headers.authorization.split(' ')[1];
-      console.log(accessToken);
-      const a = jwt.verify(accessToken, 'myAccessKey');
-      console.log(a);
-      return await this.paymentService.create({ createPaymentInput });
-    } catch {
-      throw new UnauthorizedException('유효하지 않은 토큰입니다.');
-    }
+    return await this.paymentService.create({ loginUser, createPaymentInput });
   }
 
-  // @Mutation(() => Boolean)
-  // cancelPayment(
-  //   @Args('impUid') impUid: string, //
-  // ) {
-  //   return this.paymentService.cancelPayment({ impUid });
-  // }
+  @UseGuards(GqlAuthAccessGuard)
+  @Mutation(() => Boolean)
+  async cancelPayment(
+    @Args('impUid') impUid: string, //
+  ) {
+    return await this.paymentService.cancelPayment({ impUid });
+  }
 }
