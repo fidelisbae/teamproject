a067f480f409d9e81031a1f9d91d05b068f8c709
이샘미 <isaemmi@isaemmiui-MacBookPro.local>
2022-07-19 10:10:29 +0900

    file수정

--- src/apis/file/file.module.ts
+++ src/apis/file/file.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { CourseModule } from '../course/course.module';
-import { Image } from '../image/entities/image.entity';
 import { FileResolver } from './file.resolver';
 import { FileService } from './file.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Image]), CourseModule],
   providers: [FileResolver, FileService],
 })
 export class FileModule {}
--- src/apis/file/file.service.ts
+++ src/apis/file/file.service.ts
@@ -1,40 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { Storage } from '@google-cloud/storage';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Image } from '../image/entities/image.entity';
-import { CourseService } from '../course/course.service';
 
 @Injectable()
 export class FileService {
-  constructor(
-    @InjectRepository(Image)
-    private readonly imageRepository: Repository<Image>,
-    private readonly courseService: CourseService,
-
-  ) {}
-  async upload(files) {
-
+  async upload({ files }) {
     const waitedFiles = await Promise.all(files);
+    console.log(waitedFiles);
 
     const storage = new Storage({
       projectId: 'dabae-355905',
-      keyFilename: './key/gcp-file-storage.json',
+      keyFilename: 'gcp-file-storage.json',
     }).bucket('dabaeimage');
 
-    const results = (
-      (await Promise.all(
-        waitedFiles.map((el) => {
-          return new Promise((resolve, reject) => {
-            el.createReadStream() //el.filename이 파일 이름 변경하는 것
-              .pipe(storage.file(el.filename).createWriteStream())
-              .on('finish', () => resolve(`dabaeimage/${el.filename}`))
-              .on('error', () => reject(null));
-          });
-        }),
-      )) as string[]
-    ).filter((v) => v !== null);
-
+    const results = await Promise.all(
+      waitedFiles.map((el) => {
+        return new Promise((resolve, reject) => {
+          el.createReadStream()
+            .pipe(storage.file(el.filename).createWriteStream())
+            .on('finish', () => resolve(`dabaeimage/${el.filename}`))
+            .on('error', () => reject());
+        });
+      }),
+    );
     return results;
   }
 }
