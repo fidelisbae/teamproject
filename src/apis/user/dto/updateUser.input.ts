20324fe5b32314732472693de1c2f3c6650d284a
fidelisbae <fidelisbae@gmail.com>
2022-07-22 13:43:36 +0900

    fix: updateUserInput: 업데이트불가항목제거

--- src/apis/user/dto/updateUser.input.ts
+++ src/apis/user/dto/updateUser.input.ts
@@ -2,7 +2,25 @@ import { Field, InputType, PartialType } from '@nestjs/graphql';
 import { CreateUserInput } from './createUser.input';
 
 @InputType()
-export class UpdateUserInput extends PartialType(CreateUserInput) {
+export class UpdateUserInput {
+  @Field(() => String)
+  nickname: string;
+
+  @Field(() => Boolean)
+  marketingAgreement: boolean;
+
+  @Field(() => String, { nullable: true })
+  account: string;
+
+  @Field(() => String, { nullable: true })
+  bank: string;
+
+  @Field(() => String, { nullable: true })
+  businessName: string;
+
+  @Field(() => String, { nullable: true })
+  businessNumber: string;
+
   @Field(() => Boolean, { nullable: true })
   gender: boolean;
 
